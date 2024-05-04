ddb105d3a63e1dd7ccb77b2075e02f703217ff9d
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-05-04 16:12:06 +0500

    Added endpoints /hello/world and /counter

--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -8,3 +8,14 @@ app = Flask(__name__)
 def test_function():
     now = datetime.datetime.now().utcnow()
     return f'Это тестовая страничка, ответ сгенерирован в {now}'
+
+
+@app.route('/hello/world')
+def hello_world():
+    return f'Hello, world!'
+
+
+@app.route('/counter')
+def counter():
+    counter.__count = counter.__count + 1 if hasattr(counter, '__count') else 1
+    return f'Данная страница открывалась {counter.__count} раз'
