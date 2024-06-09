0287f1f6c9cf79a1ec3a7109d71f02f317cf97fa
vladimir <volosnikovvladimir69@gmail.com>
2024-06-09 21:41:27 +0500

    Rewrite task 1

--- module_03_ci_culture_beginning/homework/hw1/test_hello_world_with_day.py
+++ module_03_ci_culture_beginning/homework/hw1/test_hello_world_with_day.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from freezegun import freeze_time
+from datetime import datetime, timedelta
 
 from module_03_ci_culture_beginning.homework.hw1.hello_word_with_day import app
 
@@ -13,76 +14,44 @@ class TestHelloWorldWithDay(TestCase):
         app.config['DEBUG'] = False
         cls.app = app.test_client()
         cls.base_url = '/hello-world/'
+        cls.greetings: tuple[str, ...] = (
+            'Хорошего понедельника',
+            'Хорошего вторника',
+            'Хорошей среды',
+            'Хорошего четверга',
+            'Хорошей пятницы',
+            'Хорошей субботы',
+            'Хорошего воскресенья'
+        )
 
-    @freeze_time('2024-06-03')
-    def test_correctness_receiving_today_monday(self):
-        """Проверка корректности определения понедельника"""
-        username: str = 'username'
-        url: str = self.base_url + username
-        response = self.app.get(url)
-        response_text: str = response.data.decode()
-        correct_answer: str = f'Привет, {username}. Хорошего понедельника!'
-        self.assertEqual(response_text, correct_answer)
+    @classmethod
+    def now_with_offset(cls, offset: int, now: datetime = datetime.now()) -> datetime:
+        """
+        Метод возвращает день, который наступит через offset дней после now
+        :param offset: Количество дней после now
+        :type offset: int
+        :param now: текущий день
+        :type now: datetime
+        :return: Дату через offset после now
+        :rtype: datetime
+        """
+        return now + timedelta(days=offset)
 
-    @freeze_time('2024-06-04')
-    def test_correctness_receiving_today_tuesday(self):
-        """Проверка корректности определения вторника"""
+    def test_can_get_correct_weekday(self):
+        """Проверка получения корректной даты"""
         username: str = 'username'
-        url: str = self.base_url + username
-        response = self.app.get(url)
-        response_text: str = response.data.decode()
-        correct_answer: str = f'Привет, {username}. Хорошего вторника!'
-        self.assertEqual(response_text, correct_answer)
 
-    @freeze_time('2024-06-05')
-    def test_correctness_receiving_today_wednesday(self):
-        """Проверка корректности определения среды"""
-        username: str = 'username'
-        url: str = self.base_url + username
-        response = self.app.get(url)
-        response_text: str = response.data.decode()
-        correct_answer: str = f'Привет, {username}. Хорошей среды!'
-        self.assertEqual(response_text, correct_answer)
+        for delta_day in range(7):
+            date: datetime = self.now_with_offset(delta_day)
+            weekday: int = date.weekday()
 
-    @freeze_time('2024-06-06')
-    def test_correctness_receiving_today_thursday(self):
-        """Проверка корректности определения четверга"""
-        username: str = 'username'
-        url: str = self.base_url + username
-        response = self.app.get(url)
-        response_text: str = response.data.decode()
-        correct_answer: str = f'Привет, {username}. Хорошего четверга!'
-        self.assertEqual(response_text, correct_answer)
-
-    @freeze_time('2024-06-07')
-    def test_correctness_receiving_today_friday(self):
-        """Проверка корректности определения пятницы"""
-        username: str = 'username'
-        url: str = self.base_url + username
-        response = self.app.get(url)
-        response_text: str = response.data.decode()
-        correct_answer: str = f'Привет, {username}. Хорошей пятницы!'
-        self.assertEqual(response_text, correct_answer)
-
-    @freeze_time('2024-06-08')
-    def test_correctness_receiving_today_saturday(self):
-        """Проверка корректности определения субботы"""
-        username: str = 'username'
-        url: str = self.base_url + username
-        response = self.app.get(url)
-        response_text: str = response.data.decode()
-        correct_answer: str = f'Привет, {username}. Хорошей субботы!'
-        self.assertEqual(response_text, correct_answer)
-
-    @freeze_time('2024-06-09')
-    def test_correctness_receiving_today_sunday(self):
-        """Проверка корректности определения воскресенья"""
-        username: str = 'username'
-        url: str = self.base_url + username
-        response = self.app.get(url)
-        response_text: str = response.data.decode()
-        correct_answer: str = f'Привет, {username}. Хорошего воскресенья!'
-        self.assertEqual(response_text, correct_answer)
+            with (self.subTest(msg=f'Weekday is {weekday}'), freeze_time(date)):
+                url: str = self.base_url + username
+                response = self.app.get(url)
+                response_text: str = response.data.decode()
+                self.assertIn(response_text,
+                              (f'Привет, {username}. {greeting}!'
+                               for greeting in self.greetings))
 
     def test_username_is_greetings(self):
         """Проверка ответа на ввод пожелания хорошего дня в username"""
