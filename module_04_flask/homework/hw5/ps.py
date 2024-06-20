af7876eba784b4903c6cb16cd6535102d7b5315c
Volosnikov Vladimir <volosnikovvladimir69@gmail.com>
2024-06-20 19:36:29 +0500

    Done task 5

--- module_04_flask/homework/hw5/ps.py
+++ module_04_flask/homework/hw5/ps.py
@@ -8,14 +8,33 @@
 /ps?arg=a&arg=u&arg=x
 """
 
-from flask import Flask
+from flask import Flask, request
+from sh import Command, ErrorReturnCode
+from typing import Optional
 
 app = Flask(__name__)
 
 
 @app.route("/ps", methods=["GET"])
-def ps() -> str:
-    ...
+def ps() -> tuple[str, int]:
+    """Функция - эндпоинт. Получает список аргументов для командной строки и возвращает результат работы команды
+     ps с этими аргументами"""
+    args: list[str] = request.args.getlist('arg')
+
+    try:
+        ps_cmd: Command = Command('ps')
+        ps_result: list[str] = list()
+
+        def save_result(result) -> None:
+            """Костыль. Пробовал сделать конвейер сразу в переменную uptime_result, но не получилось"""
+            nonlocal ps_result
+            ps_result.append(result)
+
+        ps_cmd(*args, _out=save_result)
+
+        return f'<pre>{"<br>".join(ps_result)}</pre>', 200
+    except ErrorReturnCode:
+        return 'Переданы неправильные параметры!', 400
 
 
 if __name__ == "__main__":
