a43e12f105c1bfafb863eb4deab279a6edcd36ec
Volosnikov Vladimir <volosnikovvladimir69@gmail.com>
2024-06-20 19:11:13 +0500

    Done task 4

--- module_04_flask/homework/hw4/uptime.py
+++ module_04_flask/homework/hw4/uptime.py
@@ -6,13 +6,30 @@
 """
 
 from flask import Flask
+from sh import Command, RunningCommand
+from typing import Optional
+
 
 app = Flask(__name__)
 
 
 @app.route("/uptime", methods=['GET'])
-def uptime() -> str:
-    ...
+def uptime() -> tuple[str, int]:
+    """
+    Функция - эндпоинт. По запросу возвращает uptime системы
+    :return:
+    """
+    uptime_command: Command = Command('uptime')
+    uptime_result: Optional[str] = None
+
+    def save_result(result) -> None:
+        """Костыль. Пробовал сделать конвейер сразу в переменную uptime_result, но не получилось"""
+        nonlocal uptime_result
+        uptime_result = result
+
+    uptime_command('-p', _out=save_result)
+
+    return f'Время работы системы: {uptime_result}', 200
 
 
 if __name__ == '__main__':
