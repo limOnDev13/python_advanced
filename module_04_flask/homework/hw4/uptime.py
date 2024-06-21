296d98b2e978ddb37e096d3656b5ceeb19d847d9
Volosnikov Vladimir <volosnikovvladimir69@gmail.com>
2024-06-21 08:22:02 +0500

    Fixed the solution to task 4

--- module_04_flask/homework/hw4/uptime.py
+++ module_04_flask/homework/hw4/uptime.py
@@ -6,30 +6,25 @@
 """
 
 from flask import Flask
-from sh import Command
-from typing import Optional
+import subprocess
+from subprocess import CompletedProcess
+import shlex
 
 
 app = Flask(__name__)
 
 
 @app.route("/uptime", methods=['GET'])
-def uptime() -> tuple[str, int]:
+def _uptime() -> tuple[str, int]:
     """
     Функция - эндпоинт. По запросу возвращает uptime системы
     :return:
     """
-    uptime_command: Command = Command('uptime')
-    uptime_result: list[str] = list()  # здесь будут храниться строки результата работы upotime
-
-    def save_result(result) -> None:
-        """Функция получает построчно результат команды uptime и сохраняет его"""
-        nonlocal uptime_result
-        uptime_result.append(result)
-
-    uptime_command('-p', _out=save_result)
-
-    return f'<pre>{"<br>".join(uptime_result)}</pre>', 200
+    command_str: str = 'uptime -p'
+    command: list[str] = shlex.split(command_str)
+    uptime: CompletedProcess = subprocess.run(command, capture_output=True)
+    time: str = uptime.stdout.decode().lstrip('up ')
+    return f'Время работы системы: {time}', 200
 
 
 if __name__ == '__main__':
