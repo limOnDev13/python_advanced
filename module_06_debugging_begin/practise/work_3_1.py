7753ccd390ddafd06ab62d6e1619f902626c7e4f
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-06-24 17:12:37 +0500

    Done task 3_1

--- module_06_debugging_begin/practise/work_3_1.py
+++ module_06_debugging_begin/practise/work_3_1.py
@@ -16,21 +16,29 @@ logger = logging.getLogger("password_checker")
 def input_and_check_password():
     logger.debug("Начало input_and_check_password")
     password: str = getpass.getpass()
+    logger.debug('Пользователь ввел пароль')
 
     if not password:
         logger.warning("Вы ввели пустой пароль.")
+        logger.debug('Пользователь ввел пустой пароль. input_and_check_password() вернула False')
         return False
 
     try:
         hasher = hashlib.md5()
+        logger.debug('Создали hasher')
 
         hasher.update(password.encode("latin-1"))
+        logger.debug('Выполнили команду hasher.update(password.encode("latin-1"))')
 
         if hasher.hexdigest() == "098f6bcd4621d373cade4e832627b4f6":
+            logger.debug('Зашли в условие if hasher.hexdigest() == "098f6bcd4621d373cade4e832627b4f6"'
+                         ' - input_and_check_password вернет True')
             return True
     except ValueError as ex:
+        logger.debug('Попали в блок except ValueError as ex')
         logger.exception("Вы ввели некорректный символ ", exc_info=ex)
 
+    logger.debug('Функция дошла до конца. input_and_check_password вернет False')
     return False
 
 
@@ -38,12 +46,17 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logger.info("Вы пытаетесь аутентифицироваться в Skillbox")
     count_number: int = 3
+    logger.debug('Создали count_number: int = 3')
     logger.info(f"У вас есть {count_number} попыток")
 
     while count_number > 0:
+        logger.debug('Зашли в блок while count_number > 0')
         if input_and_check_password():
+            logger.debug('Зашли в блок if input_and_check_password(). Программа завершится с кодом 0')
             exit(0)
         count_number -= 1
+        logger.debug('Уменьшили count_number на 1')
 
     logger.error("Пользователь трижды ввёл не правильный пароль!")
+    logger.debug('Программа дошла до конца. Сейчас она завершится с кодом 1')
     exit(1)
