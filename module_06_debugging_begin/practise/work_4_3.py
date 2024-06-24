faf9a532a598e5a3d433b830a9890a4de6a345ba
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-06-24 21:17:42 +0500

    Done task 4_3

--- module_06_debugging_begin/practise/work_4_3.py
+++ module_06_debugging_begin/practise/work_4_3.py
@@ -15,8 +15,10 @@
 """
 
 import json
+from json.decoder import JSONDecodeError
 import logging
 import os
+from datetime import datetime
 
 from flask import Flask
 
@@ -41,13 +43,41 @@ def account(department: str, account_number: int):
 
     account_data_file = os.path.join(full_department_path, f"{account_number}.json")
 
-    with open(account_data_file, "r") as fi:
-        account_data_txt = fi.read()
+    try:
+        with open(account_data_file, "r", encoding='utf-8') as fi:
+            account_data_txt = fi.read()
+    except FileNotFoundError:
+        logger.error(f'Account number not found: {account_number}')
+        return f'Account number not found: {account_number}', 400
 
-    account_data_json = json.loads(account_data_txt)
+    try:
+        account_data_json = json.loads(account_data_txt)
 
-    name, birth_date = account_data_json["name"], account_data_json["birth_date"]
-    day, month, _ = birth_date.split(".")
+        name, birth_date = account_data_json["name"], account_data_json["birth_date"]
+        if name == '':
+            logger.error('field "name" can not be empty!')
+            return 'field "name" can not be empty!', 400
+        if birth_date == '':
+            logger.error('field "birth_date" can not be empty!')
+            return 'field "birth_date" can not be empty!', 400
+
+        day, month, year = birth_date.split(".")
+        birth_datetime: datetime = datetime(int(year), int(month), int(day))
+        if birth_datetime >= datetime.now():
+            logger.error(f'Birth date can not be more than today date! birth_date = {birth_date}')
+            return f'Birth date can not be more than today date! birth_date = {birth_date}', 400
+    except JSONDecodeError:
+        logger.error(f'Not able decode json: {account_data_file}')
+        return f'Not able decode json: {account_data_file}', 400
+    except ValueError:
+        logger.error(f'Invalid birth date: {birth_date}')
+        return f'Invalid birth date: {birth_date}', 400
+    except KeyError:
+        logger.error('Invalid form:\nRequired json: {}\nActual json: {}'.format(
+            {'name': '...', 'birth_date': '...'}, account_data_json))
+        return 'Invalid form:<br>Required json: {}<br>Actual json: {}'.format(
+            {'name': '...', 'birth_date': '...'}, account_data_json), 400
+
     return f"{name} was born on {day}.{month}"
 
 
