a17b26e88e334b749c4bcdea3b8eca75a370f4f0
Volosnikov Vladimir <volosnikovvladimir69@gmail.com>
2024-06-24 09:18:49 +0500

    Done task 5

--- module_05_processes_and_threads/homework/hw5_add/self_printing.py
+++ module_05_processes_and_threads/homework/hw5_add/self_printing.py
@@ -7,4 +7,15 @@ result = 0
 for n in range(1, 11):
     result += n ** 2
 
-# Secret magic code
+
+# До сюда код может быть любым (работающим)
+import os
+
+
+if __name__ == '__main__':
+    base_dir: str = os.path.dirname(os.path.abspath(__file__))
+    file_name: str = 'self_printing.py'
+
+    with open(os.path.join(base_dir, file_name), 'r') as file:
+        for line in file:
+            print(line, end='')
