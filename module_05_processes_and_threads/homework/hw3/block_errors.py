b8acb494857735f2b107b2a548090cc0bfe001e4
Volosnikov Vladimir <volosnikovvladimir69@gmail.com>
2024-06-24 09:13:04 +0500

    Done task 3

--- module_05_processes_and_threads/homework/hw3/block_errors.py
+++ module_05_processes_and_threads/homework/hw3/block_errors.py
@@ -8,11 +8,17 @@ from types import TracebackType
 
 
 class BlockErrors:
+    """
+    Контекстный менеджер. Принимает список исключений и игнорирует их внутри себя
+
+    Args:
+        errors (Collection) - исключения
+    """
     def __init__(self, errors: Collection) -> None:
-        ...
+        self.errors: Collection = errors
 
     def __enter__(self) -> None:
-        ...
+        pass
 
     def __exit__(
             self,
@@ -20,4 +26,15 @@ class BlockErrors:
             exc_val: BaseException | None,
             exc_tb: TracebackType | None
     ) -> Literal[True] | None:
-        ...
+        if exc_type is not None and (exc_type in self.errors or issubclass(exc_type, tuple(self.errors))):
+            return True
+        elif exc_type is not None:
+            raise exc_val
+
+
+if __name__ == '__main__':
+    err_types = {ZeroDivisionError, TypeError}
+    with BlockErrors(err_types):
+        a = 1 / 0
+    print('Выполнено без ошибок')
+
