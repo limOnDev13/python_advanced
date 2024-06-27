abc1d0a7c171b2cb2ffe9482f78619460239dd88
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-06-27 17:36:32 +0500

    Done task 5

---
+++ module_07_logging_part_2/homework/hw5_rotating_file_handler/app.py
@@ -0,0 +1,48 @@
+from logger_helper import get_logger
+from utils import string_to_operator
+import logging.config
+from logging_config import dict_config
+
+
+logging.config.dictConfig(dict_config)
+
+
+app_logger = get_logger('app')
+
+
+def calc(args):
+    # print("Arguments: ", args)
+    app_logger.debug(f"Arguments: {args}")
+
+    num_1 = args[0]
+    operator = args[1]
+    num_2 = args[2]
+
+    try:
+        num_1 = float(num_1)
+    except ValueError as e:
+        # print("Error while converting number 1")
+        # print(e)
+        app_logger.exception('Error while converting number 1', exc_info=e)
+
+    try:
+        num_2 = float(num_2)
+    except ValueError as e:
+        # print("Error while converting number 1")
+        # print(e)
+        app_logger.exception('Error while converting number 1', exc_info=e)
+
+    operator_func = string_to_operator(operator)
+
+    result = operator_func(num_1, num_2)
+
+    # print("Result: ", result)
+    # print(f"{num_1} {operator} {num_2} = {result}")
+    app_logger.debug(f'Result: {result}')
+    app_logger.info(f"{num_1} {operator} {num_2} = {result}")
+
+
+if __name__ == '__main__':
+    # calc(sys.argv[1:])
+    calc('2+3')
+    calc('2==3')
