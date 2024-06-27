44c97bc4530f679cdeb1238a0afee0875fb6d9c1
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-06-27 17:11:05 +0500

    Done task 3

--- module_07_logging_part_2/homework/hw3_level_file_handler/logger_helper.py
+++ module_07_logging_part_2/homework/hw3_level_file_handler/logger_helper.py
@@ -1,12 +1,24 @@
 import logging
 import sys
+from logging import LogRecord
 
 
 class LevelFileHandler(logging.Handler):
-    ...
+    def emit(self, record: LogRecord) -> None:
+        """Сообщение в зависимости от уровня будут отправляться в соответствующие файлы"""
+        with open(f'{record.funcName}_{record.levelname}.log', 'a', encoding='utf-8') as log_file:
+            message = self.format(record)
+
+            log_file.write(message + '\n')
 
 
 def get_logger(name):
-    ...
-    return logger
+    logger = logging.getLogger(name)
+    formatter = logging.Formatter(fmt='%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s')
+    handler = LevelFileHandler()
+    handler.setLevel('DEBUG')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel('DEBUG')
 
+    return logger
