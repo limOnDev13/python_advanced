f32e7d9b643e914c7226963cef3657bc56c0ed1a
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-06-29 10:57:10 +0500

    Done task 9

--- module_07_logging_part_2/homework/hw9_ini/convert_ini_to_dict.py
+++ module_07_logging_part_2/homework/hw9_ini/convert_ini_to_dict.py
@@ -17,19 +17,26 @@ def get_items_from_section_with_key(config: configparser.ConfigParser,
     """
     result_dict: dict = dict()
 
+    # Проверка, что секция с названием section_name есть в ini файле
     if not config.has_section(section_name):
         raise IndexError(f'Секции {section_name} нет в конфиге')
 
+    # Перебираем опции у переданной секции
     for option in config[section_name]:
+        # Если опция - args - данные переда.ются в виде "(...,)" - раскроем и переместим в список
         if option == 'args':
             result_dict[option] = re.findall(r"[^()',]+", config[section_name][option])
-        if option == 'propagate':
+        # Если опция - propagate, то ее значение передаются в виде "0".
+        # Не знаю, сработает ли это в словаре, на всякий переведу в bool значение
+        elif option == 'propagate':
             if config[section_name][option] == '0':
                 result_dict[option] = False
             else:
                 result_dict[option] = True
+        # Если данные переданны через запятую (...,...) - разделим их с помощью split
         elif ',' in config[section_name][option]:
             result_dict[option] = config[section_name][option].split(',')
+        # Другие случаи не смог придумать или выцепить из примера ini файла
         else:
             result_dict[option] = config[section_name][option]
 
@@ -37,6 +44,13 @@ def get_items_from_section_with_key(config: configparser.ConfigParser,
 
 
 def compile_dict_params(config: configparser.ConfigParser, param: str) -> dict:
+    """
+    Функция собирает словарь конфигурации для каждого параметра,
+     у которого есть ссылки на другие параметры в виде опции keys
+    :param config: Конфиг объект
+    :param param: Параметр
+    :return: Словарь для параметра, ссылающегося на другие параметры
+    """
     result_dict: dict = dict()
 
     if config.has_section(param):
@@ -44,7 +58,8 @@ def compile_dict_params(config: configparser.ConfigParser, param: str) -> dict:
         if config.has_option(param, 'keys'):
             for key in config[param]['keys'].split(','):
                 result_dict[key] = get_items_from_section_with_key(config, f'{param[:-1]}_{key}')
-
+    else:
+        raise IndexError(f'Секции {param} нет в конфиге')
     return result_dict
 
 
@@ -88,6 +103,7 @@ def convert_ini_config_to_dict(ini_file: str = 'logging_conf.ini',
         # Логгеры
         result_dict['loggers'] = compile_dict_params(config, 'loggers')
 
+    # Если был передан file_with_dict, то сохраним его
     if file_with_dict:
         with open(file_with_dict, 'w', encoding='utf-8') as output:
             output.write(f'config_dict: dict = {result_dict}')
