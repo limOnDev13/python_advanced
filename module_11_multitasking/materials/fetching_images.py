96e8944829056867f17d9f2fb11b5864d56084d4
vladimir <volosnikovvladimir69@gmail.com>
2024-07-05 15:17:17 +0500

    Done pw_2

--- module_11_multitasking/materials/fetching_images.py
+++ module_11_multitasking/materials/fetching_images.py
@@ -22,17 +22,17 @@ def get_image(url: str, result_path: str) -> None:
         ouf.write(response.content)
 
 
-def load_images_sequential() -> None:
+def load_images_sequential(number: int) -> None:
     start: float = time.time()
-    for i in range(10):
+    for i in range(number):
         get_image(URL, OUT_PATH.format(i))
     logger.info('Done in {:.4}'.format(time.time() - start))
 
 
-def load_images_multithreading() -> None:
+def load_images_multithreading(number: int) -> None:
     start: float = time.time()
     threads: List[threading.Thread] = []
-    for i in range(10):
+    for i in range(number):
         thread = threading.Thread(target=get_image, args=(URL, OUT_PATH.format(i)))
         thread.start()
         threads.append(thread)
@@ -43,10 +43,10 @@ def load_images_multithreading() -> None:
     logger.info('Done in {:.4}'.format(time.time() - start))
 
 
-def load_images_multiprocessing() -> None:
+def load_images_multiprocessing(number: int) -> None:
     start: float = time.time()
     procs: List[multiprocessing.Process] = []
-    for i in range(10):
+    for i in range(number):
         proc = multiprocessing.Process(
             target=get_image,
             args=(URL, OUT_PATH.format(i)),
@@ -63,4 +63,10 @@ def load_images_multiprocessing() -> None:
 if __name__ == '__main__':
     if not os.path.exists('./temp'):
         os.mkdir('./temp')
-    load_images_multiprocessing()
+    for number_images in range(1, 1050, 20):
+        print(f'number_images = {number_images}')
+        # load_images_sequential(number_images)
+        load_images_multithreading(number_images)
+        load_images_multiprocessing(number_images)
+        time.sleep(1)
+        print('-' * 30)
