7a262e72c4ce5faf53eb6538dd162590c29ba41e
vladimir <volosnikovvladimir69@gmail.com>
2024-07-08 18:20:33 +0500

    Task 3 was done. I found a solution - to explicitly indicate that threads are not daemons. I also read in the documentation that you can rewrite the excepthook method - I kind of rewritten it, but it didn't seem to work. I don't understand how to check the solution - the program was already terminated by ctrl+c before the changes

--- module_12_multitasking_2/homework/hw3/main.py
+++ module_12_multitasking_2/homework/hw3/main.py
@@ -1,6 +1,14 @@
+import threading
 from threading import Semaphore, Thread
 import time
+from typing import Callable
 
+
+def custom_exception_hook(args):
+    print(f'Говорит кастомный хук: {args}')
+
+
+threading.excepthook = custom_exception_hook
 sem: Semaphore = Semaphore()
 
 
@@ -22,6 +30,9 @@ def fun2():
 
 t1: Thread = Thread(target=fun1)
 t2: Thread = Thread(target=fun2)
+t1.daemon = False
+t2.daemon = False
+
 try:
     t1.start()
     t2.start()
