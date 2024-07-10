b61c4f8bc52d31088ca6c9ea24538a3b0d2bfde8
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-07-10 13:22:24 +0500

    Done practice 13.3.2

--- module_13_db2/practice/03_02_maluksa_post.py
+++ module_13_db2/practice/03_02_maluksa_post.py
@@ -1,6 +1,11 @@
 import sqlite3
 
-sql_script_to_execute = "..."
+sql_script_to_execute = """
+UPDATE table_russian_post
+    SET order_day = replace(order_day, '-05-', '-06-')
+    WHERE order_day LIKE '%-05-%'
+"""
+
 
 if __name__ == "__main__":
     with sqlite3.connect("practise.db") as conn:
