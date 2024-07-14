216d7b3be231a29f8594805c6e99cbfe09bd9b5f
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-07-14 10:05:59 +0500

    Done task 2

--- module_14_mvc/homework/routes.py
+++ module_14_mvc/homework/routes.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired
 from typing import List
 
 from models import init_db, get_all_books, DATA
 import models
 
 app: Flask = Flask(__name__)
+
+
+class NewBookForm(FlaskForm):
+    book_title = StringField(validators=[InputRequired()])
+    author_name = StringField(validators=[InputRequired()])
 
 
 def _get_html_table_for_books(books: List[dict]) -> str:
@@ -39,13 +47,20 @@ def all_books() -> str:
 
 
 @app.route('/books/form', methods=['GET', 'POST'])
-def get_books_form() -> str:
-    if request.method == 'POST':
-        models.add_new_book(request.form['book_title'], request.form['author_name'])
-        return 'Книга добавлена в базу данных!<br><a href="http://localhost:5000/books">Список всех книг</a>'
-    return render_template('add_book.html')
+def get_books_form() -> tuple[str, int]:
+    if request.method == 'GET':
+        return render_template('add_book.html'), 200
+    else:
+        form = NewBookForm()
+
+        if form.validate_on_submit():
+            title, author = form.book_title.data, form.author_name.data
+            models.add_new_book(title, author)
+            return 'Книга добавлена в базу данных!<br><a href="http://localhost:5000/books">Список всех книг</a>', 200
+        return f'Неправильно заполненная форма! {form.errors}', 400
 
 
 if __name__ == '__main__':
     init_db(DATA)
+    app.config['WTF_CSRF_ENABLED'] = False
     app.run(debug=True)
