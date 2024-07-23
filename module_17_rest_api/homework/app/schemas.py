f51951bd35f8423fbc6c4d3c7bca270f8e2fd428
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-07-23 23:46:27 +0500

    Added AuthorSchema

--- module_17_rest_api/homework/app/schemas.py
+++ module_17_rest_api/homework/app/schemas.py
@@ -1,12 +1,30 @@
-from marshmallow import Schema, fields, validates, ValidationError, post_load
+from marshmallow import Schema, fields, validates, validates_schema, ValidationError, post_load
 
-from models import get_book_by_title, Book
+from models import get_book_by_title, Book, get_author_by_name, Author
+
+
+class AuthorSchema(Schema):
+    id = fields.Int(dump_only=True)
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    middle_name = fields.Str()
+
+    @validates_schema
+    def validate_full_name(self, data, **kwargs) -> None:
+        """Проверка наличия полного имени в бд"""
+        if get_author_by_name(data['first_name'], data['last_name'], data['middle_name']):
+            raise ValidationError(f"The author with the name {data['first_name']} {data['last_name']}"
+                                  f" is already in the database!")
+
+    @post_load
+    def create_author(self, data: dict, **kwargs) -> Author:
+        return Author(**data)
 
 
 class BookSchema(Schema):
     id = fields.Int(dump_only=True)
     title = fields.Str(required=True)
-    author = fields.Str(required=True)
+    author = fields.Nested(AuthorSchema(), only=('first_name', 'last_name', 'middle_name'))
 
     @validates('title')
     def validate_title(self, title: str) -> None:
@@ -17,5 +35,7 @@ class BookSchema(Schema):
             )
 
     @post_load
-    def create_book(self, data: dict) -> Book:
+    def create_book(self, data: dict, **kwargs) -> Book:
+        author = data.pop('author')
+        data['author_id'] = author['id']
         return Book(**data)
