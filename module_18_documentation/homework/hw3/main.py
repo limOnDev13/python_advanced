99c867389e8b6066618c3c9fb1182243cbb3941a
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-08-02 16:02:24 +0500

    Done task 3

--- module_18_documentation/homework/hw3/main.py
+++ module_18_documentation/homework/hw3/main.py
@@ -1,6 +1,18 @@
 import operator
 from flask import Flask
 from flask_jsonrpc import JSONRPC
+from flask_jsonrpc.exceptions import InvalidParamsError
+from flasgger import Swagger, APISpec
+from apispec_webframeworks.flask import FlaskPlugin
+
+
+spec = APISpec(
+    title='Calc',
+    version='1.0.0',
+    openapi_version='2.0',
+    plugins=[FlaskPlugin()]
+)
+
 
 app = Flask(__name__)
 jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
@@ -9,32 +21,131 @@ jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
 @jsonrpc.method('calc.add')
 def add(a: float, b: float) -> float:
     """
-    Пример запроса:
-
-    $ curl -i -X POST -H "Content-Type: application/json; indent=4" \
-        -d '{
-            "jsonrpc": "2.0",
-            "method": "calc.add",
-            "params": {"a": 7.8, "b": 5.3},
-            "id": "1"
-        }' http://localhost:5000/api
-
-    Пример ответа:
-
-    HTTP/1.1 200 OK
-    Server: Werkzeug/2.2.2 Python/3.10.6
-    Date: Fri, 09 Dec 2022 19:00:09 GMT
-    Content-Type: application/json
-    Content-Length: 54
-    Connection: close
-
-    {
-      "id": "1",
-      "jsonrpc": "2.0",
-      "result": 13.1
-    }
+    This is a method for adding numbers
+    ---
+    tags:
+      - calc
+    responses:
+      200:
+        description: Sum of tho numbers,
+        schema:
+          $ref: '#definitions/Jsonrpc'
     """
     return operator.add(a, b)
+
+
+@jsonrpc.method('calc.subtract')
+def sub(a: float, b: float) -> float:
+    """
+    This is a method for subtracting numbers
+    ---
+    tags:
+      - calc
+    responses:
+      200:
+        description: The difference between two numbers
+        schema:
+          $ref: '#definitions/Jsonrpc'
+    """
+    return operator.sub(a, b)
+
+
+@jsonrpc.method('calc.multiply')
+def mul(a: float, b: float) -> float:
+    """
+    This is a method for multiplication numbers
+    ---
+    tags:
+      - calc
+    responses:
+      200:
+        description: The product of two numbers
+        schema:
+          $ref: '#definitions/Jsonrpc'
+    """
+    return operator.mul(a, b)
+
+
+@jsonrpc.method('calc.divide')
+def div(a: float, b: float) -> float:
+    """
+    This is a method for dividing numbers
+    ---
+    tags:
+      - calc
+    responses:
+      200:
+        description: The quotient of two numbers
+        schema:
+          $ref: '#definitions/Jsonrpc'
+    """
+    if b == 0.0:
+        raise InvalidParamsError(message='Zero Division', code=666)
+    return operator.truediv(a, b)
+
+
+template = spec.to_flasgger(app)
+template['definitions'] = {
+    'jsonrpc_request': {
+        "type": "object",
+        "required": ['jsonrpc', 'method', 'params', 'id'],
+        'properties': {
+            'jsonrpc': {
+                'type': 'string',
+                'default': '2.0',
+                'pattern': '2.0',
+            },
+            'method': {
+                'type': 'string',
+                'enum': ['calc.add', 'calc.subtract', 'calc.multiply', 'calc.divide']
+            },
+            'params': {
+                'type': 'object',
+                'required': ['a', 'b'],
+                'properties': {
+                    'a': {'type': 'number'},
+                    'b': {'type': 'number'}
+                }
+            },
+            'id': {'type': 'string'}
+        }
+    },
+    'jsonrpc_response': {
+        "type": "object",
+        "required": ['jsonrpc', 'id'],
+        'properties': {
+            'jsonrpc': {
+                'type': 'string',
+                'default': '2.0',
+                'pattern': '2.0',
+            },
+            'result': {'type': 'number'},
+            'id': {'type': 'string'}
+        }
+    },
+}
+template['paths'] = {
+    '/api': {
+        'post': {
+            'description': 'This is JSON-RPC endpoint for working with calc',
+            'parameters': [
+                {
+                    'in': 'body',
+                    'name': 'params for calc methods',
+                    'schema': {'$ref': '#/definitions/jsonrpc_request'}
+                }
+            ],
+            'tags': ['calc'],
+            'responses': {
+                200: {
+                    'description': 'Result of calculations',
+                    'schema': {'$ref': '#/definitions/jsonrpc_response'}
+                }
+            }
+        }
+    }
+}
+swagger = Swagger(app, template=template)
 
 
 if __name__ == '__main__':
