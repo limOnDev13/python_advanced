218afb226461edeb644cf92040c2265e8a49cacf
vladimir <volosnikovvladimir69@gmail.com>
2024-08-15 11:44:26 +0500

    Added the ability to attach multiple files to an email

--- module_22_celery/homework/mail.py
+++ module_22_celery/homework/mail.py
@@ -3,34 +3,35 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-from config import SMTP_HOST, SMTP_PORT, SMTP_PASSWORD, SMTP_USER
+from config.config import MailConfig
 
 
-def send_email(order_id: str, receiver: str, filename: str):
+def send_email(order_id: str, receiver: str, images: list[str], mail_config: MailConfig):
     """
     Отправляет пользователю `receiver` письмо по заказу `order_id` с приложенным файлом `filename`
 
     Вы можете изменить логику работы данной функции
     """
-    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+    with smtplib.SMTP(mail_config.host, mail_config.port) as server:
         server.starttls()
-        server.login(SMTP_USER, SMTP_PASSWORD)
+        server.login(mail_config.user, mail_config.password)
 
         email = MIMEMultipart()
         email['Subject'] = f'Изображения. Заказ №{order_id}'
-        email['From'] = SMTP_USER
+        email['From'] = mail_config.user
         email['To'] = receiver
 
-        with open(filename, 'rb') as attachment:
-            part = MIMEBase('application', 'octet-stream')
-            part.set_payload(attachment.read())
+        for image in images:
+            with open(image, 'rb') as attachment:
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(attachment.read())
 
-        encoders.encode_base64(part)
-        part.add_header(
-            'Content-Disposition',
-            f'attachment; filename={filename}'
-        )
+            encoders.encode_base64(part)
+            part.add_header(
+                'Content-Disposition',
+                f'attachment; filename={image}'
+            )
         email.attach(part)
         text = email.as_string()
 
-        server.sendmail(SMTP_USER, receiver, text)
+        server.sendmail(mail_config.user, receiver, text)
