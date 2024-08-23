d7e5aa7d81dd76ef18af4a7fe1e45738984921f2
vladimir <volosnikovvladimir69@gmail.com>
2024-08-23 13:52:20 +0500

    Done task 1. But to see the graph, you need to restart the server

--- module_24_debugging_2/practice/task_1.py
+++ module_24_debugging_2/practice/task_1.py
@@ -1,2 +1,24 @@
-from flask import Flask
+from flask import Flask, send_file
+from pycallgraph2 import PyCallGraph
+from pycallgraph2.output import GraphvizOutput
+import os
 
+
+app = Flask(__name__)
+
+
+@app.route('/get-graph-image')
+def get_graph_image():
+    graph_path: str = './pycallgraph.png'
+    if os.path.exists(graph_path):
+        return send_file(graph_path, mimetype='image/png'), 200
+    return 'Image not found', 404
+
+@app.route('/hello')
+def hello():
+    return 'Hello, world!', 200
+
+
+if __name__ == '__main__':
+    with PyCallGraph(output=GraphvizOutput()):
+        app.run(debug=True)
