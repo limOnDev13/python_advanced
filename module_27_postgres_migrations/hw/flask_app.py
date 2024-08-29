fffd444d114157b6faef43526604c61db3acb2f6
vladimir <volosnikovvladimir69@gmail.com>
2024-08-29 13:20:38 +0500

    need to add forms

--- module_27_postgres_migrations/hw/flask_app.py
+++ module_27_postgres_migrations/hw/flask_app.py
@@ -1,13 +1,23 @@
 from flask import request
+from flask_wtf import FlaskForm
+from wtforms import Field, StringField, SelectField
 
-from module_27_postgres_migrations.hw import app
+from module_27_postgres_migrations.hw import app, session
+from models import User
+
+
+
 
 
 @app.route('/users', methods=['POST'])
 def add_user():
     """""Эндпоинт для добавления нового пользователя"""
-    form_data = request.get_json()
-    print(form_data)
+    new_user = request.get_json()
+
+    try:
+        user_model = User(name=new_user['name'],
+                          has_sale=new_user['has_sale'],
+                          address=new_user['address'])
     return '', 200
 
 
