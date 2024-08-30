a4b5b357c7b989615636c1bfc6a38e2ffc2b6931
vladimir <volosnikovvladimir69@gmail.com>
2024-08-30 19:30:15 +0500

    Done task 2

--- module_28_security/homework/task_2/hw.py
+++ module_28_security/homework/task_2/hw.py
@@ -10,3 +10,24 @@ HTML = """
 </body>
 </html>
 """
+
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def csp_defense():
+    user_input = request.args.get('user_input')
+    return HTML.format(user_input=user_input)
+
+
+@app.after_request
+def add_csp(response: Response):
+    response.headers['Content-Security-Policy'] = "default-src 'none'"
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
